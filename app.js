64283f120537966b0f49966263d8d4fbb027233d
Diego <le21080738@merida.tecnm.mx>
2025-05-08 16:09:36 -0600

    NavBar

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 // Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
