fba77b023eb3979e5230dc09d27fc2676afb826b
Diego <le21080738@merida.tecnm.mx>
2025-05-10 20:42:31 -0600

    Addpost part 2

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('/posts')
+        .then((response) => response.json())
+        .then((posts) => {
+            const postContainer = document.getElementById("posts");
+            postContainer.innerHTML = posts
+            .map(
+                (post) => `
+                <div class="post ${post.category}">
+                ${
+                    post.image
+                    ? '<img class="post-img" src="${post.image}" alt="${post.title}"'
+                    : ""
+                }
+                <h3 class="post-category">${post.category}</h3>
+                </div>
+
+                `
+            )
+            .join("");
+        })
+        .catch((error) => {
+            console.error("Error fetching posts:", error);
+            const postContainer = document.getElementById('posts');
+            postContainer.innerHTML = "<p>Error Fetching posts.</p>";
+        });
+});
